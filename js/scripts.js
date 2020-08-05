e5b5548564b3d789dfedbb63895cc07c43572418
tanphat2107 <tanphat2107@gmail.com>
2020-08-05 08:46:01 +0700

    commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var app = app || {};
+
+var spBreak = 767;
+
+app.init = function () {
+  // app.sample();
+};
+
+app.isMobile = function () {
+  return window.matchMedia('(max-width: ' + spBreak + 'px)').matches;
+};
+
+$(function () {
+  app.init();
+});
